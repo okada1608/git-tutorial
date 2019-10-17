ca0b8fd9247d7d4d1996fcfd994617e6d8e706c8
okada <j1608@matsue-ct.jp>
2019-10-17 18:33:36 +0900

    ADD feature-repair

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,13 @@
 for i in 1..100
-         if (i/3==0)&&(i/5==0) then
-                 puts "FizzBuzz"
-         elsif i/3==0 then
+        if (i/3==0)&&(i/5==0) then
+                puts "FizzBuzz"
+        elsif i/7==0 then
+		puts "git" 
+	elsif i/3==0 then
                  puts "Fizz"
-         elsif i/5==0 then
-                 puts "Buzz"
-         else
-                 printf("%d\n",i)
-         end
+        elsif i/5==0 then
+                puts "Buzz"
+        else
+                printf("%d\n",i)
+        end
 end
