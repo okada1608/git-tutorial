db09e72c83b97e9f66929901180aca982b8b1161
okada <j1608@matsue-ct.jp>
2019-10-17 18:23:38 +0900

    2019-10-17 modified by okada

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,7 @@
 for i in 1..100
-	if (i/3==0)&&(i/5==0) then
-		puts "FizzBuzz"
-	elsif i/3==0 then
-		puts "Fizz"
-	elsif i/5==0 then
-		puts "Buzz"
-	else
-		printf("%d\n",i)
-	end
-end
+         if (i/3==0)&&(i/5==0) then
+                 puts "FizzBuzz"
+         elsif i/3==0 then
+                 puts "Fizz"
+         elsif i/5==0 then
+                 puts "Buzz"  
